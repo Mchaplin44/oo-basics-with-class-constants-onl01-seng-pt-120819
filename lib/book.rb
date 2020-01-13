bef8f72806503a1fccc3ad2c966caea7b9a7169a
Martin Chaplin <martin.chaplin44@gmail.com>
2020-01-13 01:53:34 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,6 @@
 class Book
   attr_accessor :author, :page_count, 
-  attr_reader :title,
+  attr_reader :title,:genre
   
   GENRES = []
 
@@ -12,4 +12,3 @@ class Book
     puts "Flipping the page...wow, you read fast!"
   end
 
-end
